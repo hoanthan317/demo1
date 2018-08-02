fcb06ff6d2b943715ea40b6da5c654c8c13334bb
Hoan Than <hoanthan317@gmail.com>
2018-08-02 17:46:37 +0700

    Update

--- client/src/components/layouts/Home.js
+++ client/src/components/layouts/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                Test layout
+                Home page
                 <News/>
             </div>
         );
