83ddbff94b93e1f37076ed4fcec44dab10c21776
Hoan Than <hoanthan317@gmail.com>
2018-08-03 09:11:06 +0700

    update 2018/08/03

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './components/layouts/Home';
 import About from './components/layouts/About';
 import Layout from './components/layouts/Layout';
@@ -8,16 +8,25 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import NewsItemArticle from './components/layouts/NewsItemArticle';
 
+const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
+  <Route {...rest} render={props => (
+    <Layout>
+      <Component {...props} />
+    </Layout>
+  )} />
+)
+
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <Layout>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About} />
-            <Route path="/news/:id" component={NewsItemArticle}/>
-          </Layout>
+          <Switch>
+            <AppRoute exact path="/" layout={Layout} component={Home}/>
+            <AppRoute path="/about" layout={Layout} component={About} />
+            <AppRoute path="/news/:id" layout={Layout} component={NewsItemArticle}/>
+          </Switch>
         </BrowserRouter>
       </Provider>
     );
